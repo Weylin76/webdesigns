8d6987bf5500dbe00673dfcce656f78170ead4ee
Weylin76 <Weylin76@msn.com>
2022-04-23 19:33:55 -0400

    final changes week one

---
+++ js/currentdate.js
@@ -0,0 +1,40 @@
+// day names array
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+// long month names arrary
+const months =  [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const todaysdate = new Date();
+const dayNames = daynames[todaysdate.getDay()];
+const monthName = months[todaysdate.getMonth()];
+const month = todaysdate.getMonth();
+const day = todaysdate.getDay()
+const year = todaysdate.getFullYear();
+const hour = todaysdate.getHours();
+const minutes = todaysdate.getMinutes();
+const seconds = todaysdate.getSeconds();
+
+const currentdate = `${month}/${day}/${year} ${hour}:${minutes}:${seconds}`
+
+document.getElementById('current_date').textContent = currentdate;
