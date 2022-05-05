6b04d787aed9c8259bade5f067f476d25c311bcc
Weylin76 <Weylin76@msn.com>
2022-05-04 20:13:56 -0400

    updated lesson 2 link

--- lesson2/scripts.js
+++ lesson2/scripts.js
@@ -1,43 +1,11 @@
-// // day names array
-// const daynames = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday"
-// ];
-
-// // long month names arrary
-// const months =  [
-//     "January",
-//     "Feburary",
-//     "March",
-//     "April",
-//     "May",
-//     "June",
-//     "July",
-//     "August",
-//     "September",
-//     "October",
-//     "November",
-//     "December"
-// ];
-
-// const todaysdate = new Date();
-// const dayNames = daynames[todaysdate.getDay()];
-// const monthName = months[todaysdate.getMonth()];
-// const month = todaysdate.getMonth()+1;
-// const day = todaysdate.getDay()
-// const year = todaysdate.getFullYear();
-// const hour = todaysdate.getHours();
-// const minutes = todaysdate.getMinutes();
-// const seconds = todaysdate.getSeconds();
-
-// const currentdate = `${month}/${day}/${year} ${hour}:${minutes}:${seconds}`
-
-// document.getElementById('current_date').textContent = currentdate;
+const todaysdate = new Date();
+const month = todaysdate.getMonth()+1;
+const day = todaysdate.getDay()+1;
+const year = todaysdate.getFullYear();
+const hour = todaysdate.getHours();
+const minutes = todaysdate.getMinutes();
+const seconds = todaysdate.getSeconds();
 
 const date = new Date(document.lastModified);
-document.getElementById("currentDate").innerHTML = date;
+const currentDate = `${month}/${day}/${year} ${hour}:${minutes}:${seconds}`;
+document.getElementById("currentDate").innerHTML = currentDate;
