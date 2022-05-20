53bbe4cc41662946f84f6a38c801f5e43594eec4
Weylin76 <Weylin76@msn.com>
2022-05-20 08:53:26 -0400

    input focus not working

--- testCode/dom/js/scripts.js
+++ testCode/dom/js/scripts.js
@@ -5,6 +5,7 @@ const button = document.querySelector('button');
 button.addEventListener('click', () => {
     const myItem = input.value;
     input.value = '';
+    
 
 const listItem = document.createElement('li');
 const listText = document.createElement('span');
@@ -16,8 +17,11 @@ listItem.appendChild(listBtn);
 listBtn.textContent = 'Delete';
 list.appendChild(listItem);
 
+
 listBtn.addEventListener('click', () => {
-    list.removeChild(listItem);   
+    list.removeChild(listItem); 
+    
 })
+input.focus();
 })
 
