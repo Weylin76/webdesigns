8fa671fc2af9637ae84a80f0613ce7713461feef
Weylin76 <Weylin76@msn.com>
2022-06-03 21:14:06 -0400

    fixed windchill calculation

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -4,11 +4,11 @@
 
 let t = parseFloat(document.querySelector(".temperature").innerHTML)
 let s = parseFloat(document.querySelector(".wind-speed").innerHTML)
-let chillFactor = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16))
-if (t <= 50.0 && s > 3.0) {
-    document.querySelector(".wind-chill").innerHTML = Math.ceil(chillFactor)
+if (t >= 50.0 || s < 3.0) {
+    document.querySelector(".wind-chill").innerHTML = "N/A" 
 }
 else {
-    document.querySelector(".wind-chill-display").innerHTML = "N/A"
+    let chillFactor = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16))
+    document.querySelector(".wind-chill").innerHTML = Math.ceil(chillFactor)
 }
 
