eb444effd2a19e2ed6dc73dce5319a6f9a71fd97
Weylin76 <Weylin76@msn.com>
2022-06-20 22:40:03 -0400

    API Weather

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,17 @@
+const weatherIcon = document.querySelector('#weatherIcon');
+const captionDesc = document.querySelector('figcaption');
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,us&units=imperial&APPID=5d177bfcfd97660efa8f1485d0796cd6`
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    document.querySelector('#current-temp').textContent = data.main.temp;
+    const weatherIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const weatherDescription = data.weather[0].description;
+    document.querySelector('#icon').setAttribute('src', weatherIcon);
+    document.querySelector('#icon').setAttribute('alt', weatherDescription);
+    let revisedWeatherDescription = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1)
+    document.querySelector('figcaption').textContent = revisedWeatherDescription;
+  });
+ 
