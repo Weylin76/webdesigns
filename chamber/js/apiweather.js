7673c064a1e0f5f6788ab08e1a3984973cb9ab75
Weylin76 <Weylin76@msn.com>
2022-06-24 00:35:59 -0400

    still need icon to work on API

--- chamber/js/apiweather.js
+++ chamber/js/apiweather.js
@@ -1,5 +1,5 @@
-const weatherIcon = document.querySelector('#weatherIcon');
-const captionDesc = document.querySelector('figcaption');
+// const weatherIcon = document.querySelector('#weatherIcon');
+// const captionDesc = document.querySelector('figcaption');
 const URL = `https://api.openweathermap.org/data/2.5/weather?q=Laingsburg,us&units=imperial&APPID=5d177bfcfd97660efa8f1485d0796cd6`
 
 fetch(URL)
@@ -7,15 +7,10 @@ fetch(URL)
   .then((data) => {
     console.log(data);
     document.querySelector('#temperature').textContent = data.main.temp.toFixed(0);
+    document.querySelector('#description').innerHTML = data.weather[0].description;
     let t = (document.querySelector('#temperature').textContent);
     document.querySelector('#wind-speed').textContent = data.wind.speed;
     let s = document.querySelector('#wind-speed').textContent;
-    const weatherIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    const weatherDescription = data.weather[0].description;
-    document.querySelector('#weatherLogo').setAttribute('src', weatherIcon);
-    document.querySelector('#weatherLogo').setAttribute('alt', weatherDescription);
-    let revisedWeatherDescription = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1)
-    // document.querySelector('figcaption').textContent = revisedWeatherDescription;
     if (t >= 50.0 || s < 3.0) {
         document.querySelector(".wind-chill").innerHTML = "N/A" 
     }
@@ -23,4 +18,13 @@ fetch(URL)
         let chillFactor = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16))
         document.querySelector(".wind-chill").innerHTML = Math.ceil(chillFactor)
     }
+
+    const weatherIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const captionDesc = document.querySelector('figcaption');
+    document.querySelector('#icon').setAttribute('src', weatherIcon);
+    document.querySelector('#icon').setAttribute('alt', weatherDescription);
   });
+
+
+  //string[0].toUpperCase() + string.substring(1) 
+  //document.querySelector('figcaption').textContent = revisedWeatherDescription;
